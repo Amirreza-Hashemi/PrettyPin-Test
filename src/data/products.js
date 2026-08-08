48d48c0fd09fbb1b5aea661f5cf52889bcda76be
Amirreza Hashemi <amirrezahashemi403@gmail.com>
2026-08-08 09:39:40 +0330

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -34,38 +34,6 @@ const products = [
         image: product2,
         isFavorite: false,
     },
-    {
-        id: "p5",
-        title: "کش موی ساتن صورتی",
-        price: 259000,
-        discountPercent: 0,
-        image: product1,
-        isFavorite: false,
-    },
-    {
-        id: "p6",
-        title: "گیره سر مرواریددار",
-        price: 259000,
-        discountPercent: 20,
-        image: product2,
-        isFavorite: false,
-    },
-    {
-        id: "p7",
-        title: "هدبند مخملی مشکی",
-        price: 189000,
-        discountPercent: 0,
-        image: product1,
-        isFavorite: false,
-    },
-    {
-        id: "p8",
-        title: "سنجاق سر طلایی",
-        price: 259000,
-        discountPercent: 0,
-        image: product2,
-        isFavorite: false,
-    },
 ];
 
 export default products;
