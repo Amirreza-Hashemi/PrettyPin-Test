8cf51ace5d3a35abda4a2475c05358d51a2a283a
Amirreza Hashemi <amirrezahashemi403@gmail.com>
2026-08-08 09:18:46 +0330

    Update HeroImage.jsx

--- src/components/hero/HeroImage.jsx
+++ src/components/hero/HeroImage.jsx
@@ -4,14 +4,14 @@ const HeroImage = ({ image }) => {
       <img
         src={image}
         alt="بنر فروشگاه"
-        className="h-50 w-full object-cover sm:h-100 md:h-100"
+        className="h-[380px] w-full object-cover sm:h-[420px] md:h-[520px]"
       />
 
-      {/* موبایل: محتوا پایین عکسه، پس یه سایه‌ی یکنواخت کافیه */}
-      <div className="absolute inset-0 bg-black/45 sm:hidden" />
+      {/* موبایل: فقط پایین عکس تیره میشه، نه کل عکس */}
+      <div className="absolute inset-0 bg-gradient-to-t from-black/85 via-black/10 to-transparent sm:hidden" />
 
-      {/* دسکتاپ: محتوا سمت راسته، پس گرادیانت جهت‌دار لازمه */}
-      <div className="absolute inset-0 hidden bg-linear-to-l from-black/60 via-black/350 to-transparent sm:block" />
+      {/* دسکتاپ: گرادیانت جهت‌دار سمت راست، مثل قبل */}
+      <div className="absolute inset-0 hidden bg-gradient-to-r from-black/60 via-black/20 to-transparent sm:block" />
     </>
   );
 };
