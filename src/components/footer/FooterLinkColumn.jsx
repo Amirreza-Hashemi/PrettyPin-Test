5112d43105d8f4d553750e17a50faaf000e093aa
Amirreza Hashemi <amirrezahashemi403@gmail.com>
2026-08-08 10:19:13 +0330

    Update FooterLinkColumn.jsx

--- src/components/footer/FooterLinkColumn.jsx
+++ src/components/footer/FooterLinkColumn.jsx
@@ -1,22 +1,18 @@
 const FooterLinkColumn = ({ title, links }) => {
-    return (
-        <div>
-            <h4 className="text-sm font-bold text-white sm:text-base">{title}</h4>
-            <ul className="mt-4 flex flex-col gap-3">
-                {links.map((link) => (
-                    <li key={link}>
-                        <a
-
-                        href="#"
-                        className="text-sm text-gray-400 transition hover:text-pink-400"
-                        >
-                        {link}
-                    </a>
-                    </li>
-                    ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div dir="rtl" className="text-right">
+      <h4 className="text-sm font-bold text-white sm:text-base">{title}</h4>
+      <ul className="mt-4 flex flex-col gap-3">
+        {links.map((link) => (
+          <li key={link}>
+            <a href="#" className="text-sm text-gray-400 transition hover:text-pink-400">
+              {link}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default FooterLinkColumn;
