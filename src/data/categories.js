433ac057ffadbeca31c89ffb8b194c85dfcc2423
Amirreza Hashemi <amirrezahashemi403@gmail.com>
2026-08-08 09:35:22 +0330

    Update categories.js

--- src/data/categories.js
+++ src/data/categories.js
@@ -1,11 +1,14 @@
-import { Ribbon, Palette, Watch, Gem, Package } from "lucide-react";
+import { Sparkles, BriefcaseBusiness, Watch, Gem, Package, Palette } from "lucide-react";
 
 const categories = [
-  { id: 1, label: "اکسسوری های مو", icon: Ribbon, bgColor: "bg-pink-100" },
+  { id: 1, label: "اکسسوری مو", icon: Sparkles, bgColor: "bg-pink-100" },
   { id: 2, label: "آرایشی و مراقبتی", icon: Palette, bgColor: "bg-purple-100" },
-  { id: 3, label: "ساعت مچی", icon: Watch, bgColor: "bg-blue-100" },
-  { id: 4, label: "جواهرات و زیورآلات", icon: Gem, bgColor: "bg-yellow-100" },
-  { id: 6, label: "سایر محصولات", icon: Package, bgColor: "bg-gray-200" },
+  { id: 3, label: "ساعت و عینک", icon: Watch, bgColor: "bg-blue-100" },
+  { id: 4, label: "زیورآلات", icon: Gem, bgColor: "bg-yellow-100" },
+  { id: 5, label: "اکسسوری و لوازم کاربردی", icon: BriefcaseBusiness, bgColor: "bg-green-200" },
+  { id: 6, label: " سایر محصولات ", icon:
+Package, bgColor:"bg-gray-200"},
+
 ];
 
 export default categories;
